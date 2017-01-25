a7cd40a1c6bbf2170aa2e98b99a1eadbe3d59b29
channbora <channbora@gmail.com>
2017-01-25 14:24:47 +0700

    remove security

--- src/main/java/com/rean/spring/hibernate/configuration/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.rean.spring.hibernate.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
-	public SecurityWebApplicationInitializer() {
-        super(WebConfiguration.class);
-    }
-}
